dce37c0b5f7f2953c68337f80fe03b639ddca521
Emmanuel Pacaud <emmanuel.pacaud@lapp.in2p3.fr>
2021-10-04 21:03:17 +0200

    main: use a glib main loop

--- src/main.c
+++ src/main.c
@@ -4,38 +4,59 @@
 #include <mosquitto.h>
 #include <yawsw524.h>
 
+typedef struct {
+        GMainLoop *main_loop;
+        YawSw524 *sound_meter;
+        struct mosquitto *mqtt_client;
+} ApplicationData;
+
+static gboolean
+_read_value (gpointer user_data)
+{
+        ApplicationData *app = user_data;
+	GError *error = NULL;
+        double value;
+        YawUnit unit;
+
+        yaw_sw524_set_unit (app->sound_meter, YAW_UNIT_DB_A, &error);
+        if (error == NULL)
+                value = yaw_sw524_read_value (app->sound_meter, &unit, &error);
+
+        if (error == NULL) {
+                printf ("%.1f %s\n", value, unit == YAW_UNIT_DB_C ? "dbC" : "dbA");
+                return TRUE;
+        }
+
+        printf ("Failed: %s\n", error->message);
+
+        g_main_loop_quit (app->main_loop);
+
+        return FALSE;
+}
+
 int
 main (int argc, char **argv)
 {
 	GError *error = NULL;
-	struct mosquitto *mqtt_client = NULL;
-	YawSw524 *sonometer;
+        ApplicationData app;
 
 	mosquitto_lib_init ();
 
-	sonometer = yaw_sw524_new (&error);
+	app.sound_meter = yaw_sw524_new (&error);
+        app.mqtt_client = mosquitto_new (NULL, TRUE, NULL);
 
-	mqtt_client = mosquitto_new (NULL, TRUE, NULL);
-
-	if (error == NULL) {
-		int i;
-
-		for (i = 0; i < 100; i++) {
-			double value;
-			YawUnit unit;
-
-			value = yaw_sw524_read_value (sonometer, &unit, &error);
-
-			printf ("Success %g %s\n", value, unit == YAW_UNIT_DB_C ? "dbC" : "dbA");
-		}
-	}
-
-	if (error != NULL)
+	if (error != NULL) {
 		printf ("Failed: %s\n", error->message);
+        } else {
+                app.main_loop = g_main_loop_new (NULL, FALSE);
+                g_timeout_add_seconds (1, _read_value, &app);
 
-	g_clear_object (&sonometer);
+                g_main_loop_run (app.main_loop);
+                g_main_loop_unref (app.main_loop);
+        }
 
-	mosquitto_destroy (mqtt_client);
+	g_clear_object (&app.sound_meter);
+	mosquitto_destroy (app.mqtt_client);
 
 	mosquitto_lib_cleanup ();
 
